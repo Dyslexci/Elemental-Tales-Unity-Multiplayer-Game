a43434b302aa5be2d3e178e3ac98a8446c342e78
Dyslexci <matthewahearn2@hotmail.com>
2020-11-04 00:22:03 +0000

    Multiple additions (characters, ending, camera)
    Added in a second usable character, a smooth camera which follows the primary character and respects level boundaries, and an exit entity which will open up the end game screen and enable the user to quit the game

---
+++ Elemental Tales/Assets/Player 2 Assets/Player2Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    public CharacterControllerPlayer1 controller;
+    //public Animator animator;
+
+    private float runSpeed = 40f;
+    private float HorizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    private void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal2") * runSpeed;
+
+        //animator.SetFloat("Speed", Mathf.Abs(HorizontalMove));
+
+        if (Input.GetButtonDown("Jump2"))
+        {
+            jump = true;
+            //animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch2"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch2"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void onLanding()
+    {
+        //animator.SetBool("isJumping", false);
+    }
+
+    public void onCrouching(bool isCrouching)
+    {
+        //animator.SetBool("isCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(HorizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
