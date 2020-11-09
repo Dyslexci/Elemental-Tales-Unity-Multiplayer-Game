ad0d9c1e55213f44fe1e6d880b7981d082c73f14
himesh99 <himesh.patel99@hotmail.com>
2020-11-09 12:16:50 +0000

    Pause Menu Script

---
+++ Elemental Tales/Assets/pauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool pausedGame = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausedGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        pausedGame = true;
+    }
+
+     void Pause( )
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        pausedGame = true;
+    }
+
+    public void loadingMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void quittingMenu()
+    {
+        Debug.Log("Quitting Game....");
+        Application.Quit();
+    }
+}
