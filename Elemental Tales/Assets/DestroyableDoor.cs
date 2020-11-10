0481382a8052dc6bc45f57b02205f1a9befd6c16
Dyslexci <matthewahearn2@hotmail.com>
2020-11-10 22:40:10 +0000

    Added melee functionality

---
+++ Elemental Tales/Assets/DestroyableDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableDoor : MonoBehaviour
+{
+    private int maxHealth = 100;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void damageDoor(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            destroyDoor();
+        }
+    }
+    private void destroyDoor()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Elemental Tales/Assets/PlayerMelee.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+
+    private void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetButtonDown("Attack") && gameObject.GetComponent<CharacterControllerPlayer1>().getElement().Equals("Fire"))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+        
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<DestroyableDoor>().damageDoor(40);
+        }
+    }
+
+     private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
