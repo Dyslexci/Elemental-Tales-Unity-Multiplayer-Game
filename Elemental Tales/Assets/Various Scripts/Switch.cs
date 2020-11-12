137ba5a1bfd473499d4a19cc7904dba8910c4bf1
Dyslexci <matthewahearn2@hotmail.com>
2020-11-12 19:23:30 +0000

    Fixed errors

--- Elemental Tales/Assets/Various Scripts/Switch.cs
+++ Elemental Tales/Assets/Various Scripts/Switch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
+
 /*
  * @author Afoke Chizea
  * the script changes the animation of the
