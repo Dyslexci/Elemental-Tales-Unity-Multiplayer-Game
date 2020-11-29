e887b77ceaa1a80557cfe4630469a1bd606b1add
Dyslexci <matthewahearn2@hotmail.com>
2020-11-29 03:31:01 +0000

    Added options and exit confirmation screens

---
+++ Elemental Tales/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject startGameScreen;
+    [SerializeField] private GameObject optionsScreen;
+    [SerializeField] private GameObject exitScreen;
+    [SerializeField] private GameObject menuScreen;
+    [SerializeField] private PanelFader fader;
+
+    private void Start()
+    {
+        startGameScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        exitScreen.SetActive(false);
+        menuScreen.SetActive(true);
+    }
+
+    public void quitGame()
+    {
+        menuScreen.SetActive(false);
+        exitScreen.SetActive(true);
+    }
+
+    public void quitCancel()
+    {
+        menuScreen.SetActive(true);
+        exitScreen.SetActive(false);
+    }
+
+    public void quitConfirm()
+    {
+        Application.Quit();
+    }
+
+    public void startGame()
+    {
+        //Load up lobby manager
+    }
+
+    public void optionsMenu()
+    {
+        menuScreen.SetActive(false);
+        optionsScreen.SetActive(true);
+    }
+
+    public void optionsCancel()
+    {
+        optionsScreen.SetActive(false);
+        menuScreen.SetActive(true);
+    }
+}
---
+++ Elemental Tales/Assets/Scripts/PanelFader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelFader : MonoBehaviour
+{
+    private bool mFaded = true;
+    [SerializeField] private float m_Duration = 0.4f;
+
+    public void fade()
+    {
+        var canvGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(DoFade(canvGroup, canvGroup.alpha, mFaded ? 1 : 0));
+        mFaded = !mFaded;
+    }
+
+    public IEnumerator DoFade(CanvasGroup canvGroup, float start, float end)
+    {
+        float counter = 0f;
+        while(counter < m_Duration)
+        {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(start, end, counter / m_Duration);
+            
+        }
+        yield return null;
+    }
+}
