472298bea157a2dfbbb59d28714020c4ebd8cc14
Dyslexci <matthewahearn2@hotmail.com>
2020-12-02 04:02:42 +0000

    Revert "Changing to Photon"
    This reverts commit 131dabfd96141992d735b8fe2e8f68b5b87d733d.

---
+++ Elemental Tales/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool pausedGame = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausedGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        pausedGame = false;
+    }
+
+     void Pause( )
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        pausedGame = true;
+    }
+
+    public void loadingMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void quittingMenu()
+    {
+        Debug.Log("Quitting Game....");
+        Application.Quit();
+    }
+
+    public void restartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
