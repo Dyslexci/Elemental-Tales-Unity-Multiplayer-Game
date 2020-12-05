4e7a3f08942fbcea77d087544c6644d0381429f8
Dyslexci <matthewahearn2@hotmail.com>
2020-12-05 14:54:14 +0000

    Added lobby creation and connection system, with fallbacks and failsafes

--- Elemental Tales/Assets/Scripts/Launcher.cs
+++ Elemental Tales/Assets/Scripts/Launcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 namespace Com.Team12.ElementalTales
 {
@@ -13,10 +14,14 @@ namespace Com.Team12.ElementalTales
         /// <summary>
         /// Method called when the client connects to the master servers (successful connection to online server host)
         /// </summary>
-        public override void OnConnectedToMaster()
-        {
-            Debug.Log("PUN: OnConnectedToMaster() was called by PUN");
-        }
+        //public override void OnConnectedToMaster()
+        //{
+        //    Debug.Log("PUN: OnConnectedToMaster() was called by PUN");
+        //    RoomOptions roomOptions = new RoomOptions();
+        //    roomOptions.IsVisible = false;
+        //    PhotonNetwork.JoinOrCreateRoom(connectCode, roomOptions, null);
+        //    Debug.Log("PUN: A room will be created with code " + connectCode);
+        //}
 
         /// <summary>
         /// Method called when the client disconnects from the online host server
@@ -24,13 +29,28 @@ namespace Com.Team12.ElementalTales
         /// <param name="cause"></param>
         public override void OnDisconnected(DisconnectCause cause)
         {
+            progressPanel.SetActive(false);
+            controlPanel.SetActive(true);
             Debug.LogWarningFormat("PUN: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            Debug.LogErrorFormat("Room creation failed with error code {0} and error message {1}", returnCode, message);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN: OnJoinedRoom() called by PUN. Now this client is in a room.");
         }
 
         #endregion
 
         #region Private Serializable Fields
 
+        [SerializeField] private byte maxPlayersPerRoom = 2;
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private string connectCode;
 
         #endregion
 
@@ -42,6 +62,15 @@ namespace Com.Team12.ElementalTales
         /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
         /// </summary>
         string gameVersion = "1";
+        string roomCode;
+
+        [Tooltip("The UI panel to let the user enter name, connect and join")]
+        [SerializeField] private GameObject controlPanel;
+        [Tooltip("The UI panel to inform the user that the connection is in progress")]
+        [SerializeField] private GameObject progressPanel;
+        [SerializeField] private TMP_Text codeText;
+        [SerializeField] private TMP_Text roomCodeText;
+        [SerializeField] private TMP_Text connectedStatusText;
 
 
         #endregion
@@ -58,6 +87,15 @@ namespace Com.Team12.ElementalTales
             // #Critical
             // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
             PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+            if(PhotonNetwork.IsConnected)
+            {
+                connectedStatusText.text = "Connected to master server: " + PhotonNetwork.ServerAddress;
+            } else
+            {
+                connectedStatusText.text = "Connection to the master server has failed. Do you have an active internet connection?";
+            }
         }
 
 
@@ -66,7 +104,8 @@ namespace Com.Team12.ElementalTales
         /// </summary>
         void Start()
         {
-            Connect();
+            progressPanel.SetActive(false);
+            controlPanel.SetActive(true);
         }
 
 
@@ -83,18 +122,53 @@ namespace Com.Team12.ElementalTales
         /// </summary>
         public void Connect()
         {
+            for(int i = 0; i<8; i++)
+            {
+                connectCode += chars[Random.Range(0, chars.Length)];
+            }
+            codeText.text = connectCode;
+            progressPanel.SetActive(true);
+            controlPanel.SetActive(false);
+            Debug.Log("PUN: Connect() has been called, and a room will be created with code " + connectCode);
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
-            if (PhotonNetwork.IsConnected)
+            //if (PhotonNetwork.IsConnected)
+            //{
+            RoomOptions roomOptions = new RoomOptions();
+                roomOptions.IsVisible = false;
+                PhotonNetwork.JoinOrCreateRoom(connectCode, roomOptions, null);
+                
+            //}
+            //else
+            //{
+            //    // #Critical, we must first and foremost connect to Photon Online Server.
+            //    PhotonNetwork.ConnectUsingSettings();
+            //    PhotonNetwork.GameVersion = gameVersion;
+            //    Debug.Log("PUN: Connect() has been called but the client is not connected. Connecting now.");
+            //}
+        }
+
+        public void joinRoom(string roomCode)
+        {
+            if(roomCode !=  null)
             {
-                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
-                PhotonNetwork.JoinRandomRoom();
+                RoomOptions roomOptions = new RoomOptions();
+                roomOptions.IsVisible = false;
+                PhotonNetwork.JoinOrCreateRoom(roomCode, roomOptions, null);
+                Debug.Log("PUN: joinRoom() has been called, and the client will join room ID " + roomCode);
             }
-            else
+            Debug.Log("PUN: joinRoom() has been called but there is no input code.");
+        }
+
+        public void SetRoomCode(string value)
+        {
+            // #Important
+            if (string.IsNullOrEmpty(value))
             {
-                // #Critical, we must first and foremost connect to Photon Online Server.
-                PhotonNetwork.ConnectUsingSettings();
-                PhotonNetwork.GameVersion = gameVersion;
+                Debug.LogError("RoomCode is null or empty");
+                return;
             }
+            roomCode = value;
+            roomCodeText.text = roomCode;
         }
 
 
