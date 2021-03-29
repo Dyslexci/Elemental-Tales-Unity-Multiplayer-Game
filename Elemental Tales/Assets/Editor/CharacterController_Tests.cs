537e1703b60db8aeb9063c29baa9a5238be709d1
Dyslexci <matthewahearn2@hotmail.com>
2021-03-30 00:04:36 +0100

    Added unit test

---
+++ Elemental Tales/Assets/Editor/CharacterController_Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController_Tests
+{
+    [Test]
+    public void CharacterController_MoveFiveUnitsRight_Test()
+    {
+        CharacterControllerRaycast controller = new CharacterControllerRaycast();
+        var input = new Vector2(1, 0);
+        var moveAmount = new Vector2(5, 0);
+        var expectedMovement = new Vector2(5, 0);
+
+        var actualMovement = controller.Move(moveAmount, input);
+
+        Assert.That(actualMovement, Is.EqualTo(expectedMovement));
+    }
+}
